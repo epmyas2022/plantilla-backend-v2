5da30e1c9c708cbecc1c1dfc449f75f13458e9fa
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2024-06-11 12:59:34 -0600

    feat: :technologist: agregando middleware
    agregando middleware morgan para el server para mejorar la salida de la terminal

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,13 @@
 import Server from './config/server'
 import UtilsAssets from './config/assets/utils'
+import { defaultFormat } from './http/middlewares/morgan'
 
 function main (): void {
   const server = new Server()
-  UtilsAssets.loadBanner('banner', {
-    version: '1.0.0'
-  })
-  server.start()
+  UtilsAssets.loadBanner('banner', { version: '1.0.0' })
+  server
+    .addMiddleware(defaultFormat)
+    .start()
 }
 
 main()
