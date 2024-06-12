0391e9bb0106332c55d86241ce33807a34634f52
Isaac Castillo <isaac.castillo62055@uped.edu.sv>
2024-06-11 20:42:56 -0600

    fix: :bug: cambiando return del metodo auth y agregando nuevo
    se quito el retorno y agrego un nuevo metodo para hacer pruebas

--- app/http/controllers/auth/authController.ts
+++ app/http/controllers/auth/authController.ts
@@ -2,7 +2,11 @@ import { type Request, type Response } from 'express'
 import Controller from '../controller'
 
 export default class AuthController extends Controller {
-  static login = async (req: Request, res: Response): Promise<Response> => {
-    return res.send('Login')
+  static login = async (req: Request, res: Response): Promise<void> => {
+    res.send('Login')
+  }
+
+  static hello = async (req: Request, res: Response): Promise<void> => {
+    res.send('Hello world!')
   }
 }
